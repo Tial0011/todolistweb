2154a7928528731daf15a848898c062574b99c84
tial0011 <taiwoalex34@gmail.comgit config --global user.name tial0011git config --global user.email taiwoalex34@gmail.com>
2025-09-09 23:58:47 +0100

    i added improved the css

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const addTask = document.getElementById("addTask");
 const list = document.getElementById("list");
 const clearAllBtn = document.createElement("button");
 clearAllBtn.textContent = "Clear All";
+clearAllBtn.className = "clrbtn ";
 clearAllBtn.addEventListener("click", () => {
   list.innerHTML = "";
+  clearAllBtn.style.display = "none";
 });
 const container = document.getElementById("container");
 container.appendChild(clearAllBtn);
@@ -20,6 +22,14 @@ addTask.addEventListener("click", () => {
   todoInput.value = "";
   const checkboxbtn = document.createElement("input");
   checkboxbtn.type = "checkbox";
+  checkboxbtn.className = "boxbtn";
   li.appendChild(checkboxbtn);
   clearAllBtn.style.display = "inline-block";
+  const delbtn = document.createElement("button");
+  li.appendChild(delbtn);
+  delbtn.textContent = "x";
+  delbtn.className = "delbtn";
+  delbtn.addEventListener("click", () => {
+    li.remove();
+  });
 });
